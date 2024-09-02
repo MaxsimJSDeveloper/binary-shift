b34d7a0e1b2424914ed03580dea369aa21f63307
Vadilonische <vadilonische26@gmail.com>
2024-09-03 02:06:14 +0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
 import Modal from "./components/Modal/Modal.jsx";
-import MainPage from "./pages/MainPage.jsx";
+import MainPage from "./pages/MainPage";
+import Navigation from "./components/Navigation/Navigation";
+import SignIn from "./pages/SignInPage";
+import SignUp from "./pages/SignUpPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,7 +22,13 @@ function App() {
 
   return (
     <>
-      <MainPage />
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/SignInPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SignInPage() {
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+    </div>
+  );
+}
+
+export default SignInPage;

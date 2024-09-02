8152fde0853bd820b4dd5a7fbe097012fdf503ff
NoretsAndrii <147941173+NoretsAndrii@users.noreply.github.com>
2024-09-02 15:12:07 +0300

    Create EditTodayListModal.jsx

---
+++ src/components/EditTodayListModal/EditTodayListModal.jsx
@@ -0,0 +1,24 @@
+// При вызове компонента передать пропс onClose
+// <EditTodayListModal onClose={handleCloseModal} time={} amountWater={}/>
+
+import css from "../EditTodayListModal/EditTodayListModal.module.css";
+import AddWaterForm from "../AddWaterForm/AddWaterForm";
+
+function EditTodayListModal({ onClose, time = "7:00", amountWater = 400 }) {
+  return (
+    <div className={css.wrapper}>
+      <h2 className={css.title}>Edit the entered amount of water</h2>
+      <div className={css.editbox}>
+        <svg className={css.icon} width={36} height={36}>
+          <use xlinkHref="/src/img/symbol-defs.svg#icon-wrench" />
+        </svg>
+        <p className={css.amount}>{amountWater} ml</p>
+        <p className={css.time}>{time}</p>
+      </div>
+      <p className={css.text}>Correct entered data:</p>
+      <AddWaterForm onClose={onClose} currentTime={time} water={amountWater} />
+    </div>
+  );
+}
+
+export default EditTodayListModal;

28c79e204dbd997ee90ba3c93654c4b821746f84
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-09-02 11:31:57 +0200

    add modal

--- src/components/MonthStatsTable/MonthStatsTable.jsx
+++ src/components/MonthStatsTable/MonthStatsTable.jsx
@@ -3,6 +3,12 @@ import { HiOutlineChevronLeft } from "react-icons/hi2";
 import { HiOutlineChevronRight } from "react-icons/hi2";
 import css from "./MonthStatsTable.module.css"
 
+const ModalCalendar = ({children, x, y}) => {
+    return (<div className={css.modal} style={{top:y-188,left:x-292}}>
+        {children}
+    </div>)
+}
+
 const daysInMonth = (month, year) => {
     return new Date(year, month, 0).getDate();
 }
@@ -11,11 +17,15 @@ export default function MonthStatsTable() {
     const [month, setMonth] = useState(new Date().toLocaleString('en-Us', { month: 'long' }));
     const [year, setYear] = useState(new Date().getFullYear());
     const [currentDate, setCurrentDate] = useState(new Date());
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
 
     useEffect(() => {
         setMonth(currentDate.toLocaleString("en-Us", { month: 'long' }));
         setYear(currentDate.getFullYear());        
-    })
+    },[currentDate])
 
 
     const handleLeftButton = () => { 
@@ -32,8 +42,23 @@ export default function MonthStatsTable() {
             return newDate
         });
     }
+    const handleMouseEnter = (e) => {
+        setModalOpen(true) 
+        setX(e.clientX);
+        setY(e.clientY);       
+    }
 
-    const daysArray = Array.from({length: daysInMonth(currentDate.getMonth(), year)},(_,i)=>i+1)
+        const handleMouseLeave = () => {
+        setModalOpen(false)      
+    }
+
+    const daysArray = Array.from({ length: daysInMonth(currentDate.getMonth(), year) }, (_, i) => i + 1)
+    
+    const rate = 0;
+    const day = 0;
+    const dailyNorma = 1.5;
+    const servings = 0;
+
 
     return (
         <div className={css.calendar}>
@@ -43,11 +68,22 @@ export default function MonthStatsTable() {
                     <button className={css.button} onClick={handleLeftButton}><HiOutlineChevronLeft/></button>
                     <p>{month}, {year}</p>
                     {currentDate.getMonth() >= new Date().getMonth() ? (<button className={css.disable}><HiOutlineChevronRight/></button>):(<button className={css.button} onClick={handleRightButton}><HiOutlineChevronRight /></button>)}
-                </div>
+                </div>                
            </div>
             <ul className={css.ul}>
-                {daysArray.map(day => (<li className={css.li} key={day}>{day}</li>))}
-           </ul>
+                {daysArray.map(day => (<li className={css.li} key={day}  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                    <div className={css.liDate}>{day}</div>
+                    <p className={css.p}>{rate}%</p>                    
+                </li>))}
+            </ul>
+            {isModalOpen && <ModalCalendar x={x} y={y}>
+                <div className={css.dayInfo}>
+                    <p><span className={css.span}>{day}</span></p>
+                    <p>Daily norma: <span className={css.span}>{dailyNorma}L</span></p>
+                    <p>Fulfillment of the daily norm: <span className={css.span}>{rate}%</span></p>
+                    <p>How many servings of water: <span className={css.span}>{servings}</span></p>
+                </div>
+            </ModalCalendar>}
         </div>
     )
 }

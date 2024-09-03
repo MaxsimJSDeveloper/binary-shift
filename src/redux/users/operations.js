e479dd5ac360f634bd4cc1c7169c030daba13d17
Alexaanaax <149320517+Alexaanaax@users.noreply.github.com>
2024-09-03 18:26:14 +0200

    redux-user

--- src/redux/users/operations.js
+++ src/redux/users/operations.js
@@ -2,3 +2,70 @@ import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://binary-shift-backend.onrender.com/";
+
+export const fetchUser = createAsyncThunk(
+  "user/fetchUser",
+  async (_, thunkAPI) => {
+    try {
+      const res = await axios.get("/user");
+      return res.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response?.data || err.message);
+    }
+  }
+);
+
+export const updateUser = createAsyncThunk(
+  "user/updateUser",
+  async (updateData, thunkAPI) => {
+    try {
+      const res = await axios.patch(`/user`, updateData);
+      return res.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response?.data || err.message);
+    }
+  }
+);
+
+export const updateUserAvatar = createAsyncThunk(
+  "user/updateUserAvatar",
+  async (file, thunkAPI) => {
+    try {
+      const formData = new FormData();
+      formData.append("avatar", file);
+
+      const res = await axios.patch(`/user/avatar`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      return res.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response?.data || err.message);
+    }
+  }
+);
+
+export const requestResetEmail = createAsyncThunk(
+  "user/requestResetEmail",
+  async (email, thunkAPI) => {
+    try {
+      const res = await axios.post("/user/request-reset-email", { email });
+      return res.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response?.data || err.message);
+    }
+  }
+);
+
+export const resetPassword = createAsyncThunk(
+  "user/resetPassword",
+  async ({ token, password }, thunkAPI) => {
+    try {
+      const res = await axios.post("/user/reset-password", { token, password });
+      return res.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response?.data || err.message);
+    }
+  }
+);

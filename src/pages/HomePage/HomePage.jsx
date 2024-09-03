56ae74033a9bb303a3feae1fa681f351acfd1204
Holovko Maksym <golovkomaksim852@gmail.com>
2024-09-03 13:09:46 +0300

    coment

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,12 @@
-
 import { useEffect } from "react";
-import { toast } from 'react-hot-toast';
+import { toast } from "react-hot-toast";
 import { useSelector } from "react-redux";
 import { selectError } from "../../redux/water/selectors";
 
-import TodayWaterList from "../../components/TodayWaterList/TodayWaterList";
-import MonthStatsTable from "../../components/MonthStatsTable/MonthStatsTable";
-import WaterRatioPanel from "../../components/WaterRatioPanel/WaterRatioPanel";
-import DailyNorma from "../../components/DailyNorma/DailyNorma";
+// import TodayWaterList from "../../components/TodayWaterList/TodayWaterList";
+// import MonthStatsTable from "../../components/MonthStatsTable/MonthStatsTable";
+// import WaterRatioPanel from "../../components/WaterRatioPanel/WaterRatioPanel";
+// import DailyNorma from "../../components/DailyNorma/DailyNorma";
 import css from "./HomePage.module.css";
 
 const HomePage = () => {
@@ -22,21 +21,20 @@ const HomePage = () => {
       toast.error("Server error: Internal server error");
     }
   }, [error]);
-  
 
   return (
     <section className={css.section}>
       <div className={css.background}>
         <div className={css.container}>
           <div className={css.firstSection}>
-            <DailyNorma />
-            <WaterRatioPanel />
+            {/* <DailyNorma />
+            <WaterRatioPanel /> */}
           </div>
 
           <div className={css.containerProgressWrapper}>
             <div className={css["container-progress"]}>
-              <TodayWaterList />
-              <MonthStatsTable />
+              {/* <TodayWaterList />
+              <MonthStatsTable /> */}
             </div>
           </div>
         </div>

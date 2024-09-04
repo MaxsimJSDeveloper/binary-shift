819a6ac3b13965870634fcfd9556cbd2158d0b8f
DmytroRev <149614107+DmytroRev@users.noreply.github.com>
2024-09-04 21:57:10 +0300

    header create routers path in modal

--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -26,46 +26,4 @@
 //         toast.error("Error updating user information: " + error.message);
 //       });
 //   };
-import { Field, Formik } from "formik";
-import Modal from "../Modal/Modal";
-import css from "./SettingModal.module.css";
 
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
-  overlay: {
-    background: "#000000cd",
-  },
-};
-export default function SettingModal({ isOpen, onClose, avatarUrl, initials }) {
-  return (
-    <Modal isOpen={isOpen} onClose={onClose} style={customStyles}>
-      <div className={css.containerSettings}>
-        <div>
-          <h2>Setting</h2>
-        </div>
-        <div>
-          <p>Your photo</p>
-          {avatarUrl ? (
-            <img src={avatarUrl} alt="User Avatar" className={css.userAvatar} />
-          ) : (
-            <div className={css.lettersContainer}>
-              <p className={css.letters}>{initials}</p>
-            </div>
-          )}
-        </div>
-        <Formik>
-          <label htmlFor="">
-            <Field />
-          </label>
-        </Formik>
-      </div>
-    </Modal>
-  );
-}

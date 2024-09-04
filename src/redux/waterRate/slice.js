2d3aec3ba593b14442601af6bb79854eb8cee788
Vadilonische <vadilonische26@gmail.com>
2024-09-05 00:09:24 +0300

    slice waterRate

--- src/redux/waterRate/slice.js
+++ src/redux/waterRate/slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchWaterRate, putWaterRate } from "./operations";
 
 export const handlePending = (state) => {
   state.isLoading = true;
@@ -14,8 +15,25 @@ const waterRateSlice = createSlice({
   initialState: {
     isLoading: false,
     error: null,
+    data: null,
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchWaterRate.rejected, handleRejected)
+      .addCase(fetchWaterRate.pending, handlePending)
+      .addCase(fetchWaterRate.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.data = action.payload;
+        state.error = null;
+      })
+      .addCase(putWaterRate.rejected, handleRejected)
+      .addCase(putWaterRate.pending, handlePending)
+      .addCase(putWaterRate.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.data = action.payload;
+        state.error = null;
+      });
+  },
 });
 
 export const waterRateReducer = waterRateSlice.reducer;

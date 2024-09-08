c202838f615ffa954ca3daade2e4b287157b74c9
Holovko Maksym <golovkomaksim852@gmail.com>
2024-09-08 17:00:41 +0300

    style-fix

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,12 +2,10 @@ import WhyDrinkWater from "../../components/WhyDrinkWater/WhyDrinkWater.jsx";
 import WaterConsumptionTracker from "../../components/WaterСonsumptionTracker/WaterСonsumptionTracker.jsx";
 
 import css from "./MainPage.module.css";
-// import Header from "../../components/Header/Header.jsx";
 
 export default function MainPage() {
   return (
     <div className={css.background}>
-      {/* <Header /> */}
       <div className={css.wrap}>
         <WaterConsumptionTracker />
         <WhyDrinkWater />

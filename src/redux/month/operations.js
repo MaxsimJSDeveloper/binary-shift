8caa2cc5b9bc40344d873660d2e5ea528cacbc8b
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-09-11 10:22:21 +0200

    fix

--- src/redux/month/operations.js
+++ src/redux/month/operations.js
@@ -3,31 +3,31 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://binary-shift-backend.onrender.com/";
 
-// export const fetchMonthWater = createAsyncThunk(
-//   "month/fetchMonthWater",
-//   async ({ month,year}, { rejectWithValue }) => {
-//     try {
-//       const response = await axios.get(`/month?month=${month}&year=${year}`);
-//       return response.data.data;
-//     } catch (err) {
-//       return rejectWithValue(err.response?.data || err.message);
-//     }
-//   }
-// );
-
 export const fetchMonthWater = createAsyncThunk(
-  'monthItem/fetchMonthWater',
-  async ({ month, year }, { getState, rejectWithValue }) => {
-    const { monthItem } = getState();
-    const filterKeys = `${month}${year}`;
-    
-    if (monthItem.cache[filterKeys]) {
-      return rejectWithValue('Items already cached for this filters')
+  "monthItem/fetchMonthWater",
+  async ({ month,year}, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(`/month?month=${month}&year=${year}`);
+      return response.data.data;
+    } catch (err) {
+      return rejectWithValue(err.response?.data || err.message);
     }
-    const response = await axios.get(`/month?month=${month}&year=${year}`);   
-        
-    const data = await response.data.data;  
-    
-    return{filterKeys, data}
   }
-)
+);
+
+// export const fetchMonthWater = createAsyncThunk(
+//   'monthItem/fetchMonthWater',
+//   async ({ month, year }, { getState, rejectWithValue }) => {
+//     const { monthItem } = getState();
+//     const filterKeys = `${month}${year}`;
+    
+//     if (monthItem.cache[filterKeys]) {
+//       return rejectWithValue('Items already cached for this filters')
+//     }
+//     const response = await axios.get(`/month?month=${month}&year=${year}`);   
+        
+//     const data = await response.data.data;  
+    
+//     return{filterKeys, data}
+//   }
+// )
